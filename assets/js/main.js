85900f6d7fd4fcd7152e38c341997ddfa2c3cccb
Okesa-girl <sado.hiyoriyama@gmail.com>
2023-11-03 19:01:27 +0900

    余分なファイルを削除しました

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,28 +5,6 @@ $('.js-hamburger').on('click', function () {
     $(this).next().slideToggle();
 });
 
-// $(function() {
-//     if(window.matchMedia("(max-width: 820px)").matches){  
-//         //スマホ、タブ時の動作
-//         $('.menu-item-has-children').off('click');
-//         $('.menu-item-has-children').on('click', function() {//タイトル要素をクリックしたら
-//             $(this).toggleClass('is-activ');
-//             $(this).find('.sub-menu').stop().slideToggle();
-//             return false; //リンクの無効化
-//         });
-//     }else {//パソコン時の動作
-//         //menu-item-has-childrenクラスが付いたliタグイベントの無効化
-//         $('.menu-item-has-children').off('click');
-//         $('.menu-item-has-children').removeClass('is-activ');
-//         $('.menu-item-has-children').find('.sub-menu').css('display','');
-//         $('.menu-item-has-children').hover(
-//             function() {//タイトル要素をクリックしたら
-//                 $(this).toggleClass('is-activ');
-//                 $(this).find('.sub-menu').stop().slideToggle('fast');
-//         });
-//     }
-// });
-
 
 function mediaQueriesWin() {
     let width = $(window).width();
@@ -71,31 +49,3 @@ $(window).on('load',function() {
     mediaQueriesWin();
 })
 
-
-//ドロップダウンの設定を関数でまとめる
-// function mediaQueriesWin(){
-//     var width = $(window).width();
-//     if(width <= 768) {//横幅が768px以下の場合
-//       $(".has-child>a").off('click'); //has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
-//       $(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
-//         var parentElem =  $(this).parent();// aタグから見た親要素の<li>を取得し
-//         $(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
-//         $(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
-//         return false;//リンクの無効化
-//       });
-//     }else{//横幅が768px以上の場合
-//       $(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
-//       $(".has-child").removeClass('active');//activeクラスを削除
-//       $('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
-//     }
-//   }
-  
-//   // ページがリサイズされたら動かしたい場合の記述
-//   $(window).resize(function() {
-//     mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
-// });
-
-// // ページが読み込まれたらすぐに動かしたい場合の記述
-// $(window).on('load',function(){
-//   mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
-// });
